03f617dcb57344068df63618f2ebe773154c13ff
SearyBlue <mukherjeeabhirup44@gmail.com>
2022-01-02 22:56:20 +0530

    improved output of anime-search.py, added usage instructions

--- anime_search.py
+++ anime_search.py
@@ -11,6 +11,19 @@ from pySmartDL import SmartDL
 import subprocess
 
 def search(name,cat):
+    page = rq.get("https://myanimelist.net/anime.php?q={}&cat={}".format(name,cat))
+    soup = bs(page.content,features="lxml")
+    for title in soup.findAll('strong')[0:10]:
+        link = title.findPrevious('a')['href']
+        typ = title.findNext('td', {"class": "borderClass ac bgColor0"})
+        epi = typ.findNext('td', {"class": "borderClass ac bgColor0"})
+        score = epi.findNext('td', {"class": "borderClass ac bgColor0"})
+        print (title.text.strip(), end='\t')
+        print ("Episodes:",epi.text.strip(), end='\t')
+        print ("Score:",score.text.strip(), end='\t')
+        print ("Type:",typ.text.strip())
+        print ("Link:",link.strip())
+        print ()
     page = rq.get("https://myanimelist.net/manga.php?q={}&cat={}".format(name,cat))
     soup = bs(page.content,features="lxml")
     for title in soup.findAll('strong')[0:10]:
@@ -18,15 +31,18 @@ def search(name,cat):
         typ = title.findNext('td', {"class": "borderClass ac bgColor0"})
         epi = typ.findNext('td', {"class": "borderClass ac bgColor0"})
         score = epi.findNext('td', {"class": "borderClass ac bgColor0"})
-        print (title.text.strip())
-        print (epi.text.strip(),"\t",score.text.strip(),"\t",typ.text.strip(),"\t",link.strip())
+        print (title.text.strip(), end='\t')
+        print ("Episodes:",epi.text.strip(), end='\t')
+        print ("Score:",score.text.strip(), end='\t')
+        print ("Type:",typ.text.strip())
+        print ("Link:",link.strip())
         print ()
 
 
 n = len(sys.argv)
 cat = ""
 if n == 1:
-    print ("Enter name of anime.")
+    print ("USAGE: python <search keyword> [category]\nCategory = anime(an)/manga(ma)/all(al)")
     quit()
 elif n == 2:
     name = sys.argv[1]
